abc625e23531beab1370b3bd493e20cbf171e7e6
Francis Angelo Batiancela <107593236+kokoab@users.noreply.github.com>
2026-05-28 18:02:55 +0800

    Add: is-anagram - submission-18

---
+++ Data Structures & Algorithms/is-anagram/submission-18.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_map = defaultdict(int)
+        t_map = defaultdict(int)
+
+        for i in range(len(s)):
+            s_map[s[i]] += 1
+            t_map[t[i]] += 1
+
+        if s_map == t_map:
+            return True
+        return False
+
